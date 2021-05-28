0bd2c65a24b8e71de362f4b72286f37d8c6ce5d2
F M <francis.meyvis@sony.com>
2021-05-28 14:29:00 +0200

    Integrate provisioning

--- main/app_wifi.c
+++ main/app_wifi.c
@@ -1,17 +1,17 @@
 /* ESPRESSIF MIT License
- * 
+ *
  * Copyright (c) 2018 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
- * 
+ *
  * Permission is hereby granted for use on all ESPRESSIF SYSTEMS products, in which case,
  * it is free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
  * to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -123,8 +123,7 @@ void wifi_init_softap()
 
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
 
-    ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_AP_SSID, EXAMPLE_ESP_WIFI_AP_PASS);
+    ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s", EXAMPLE_ESP_WIFI_AP_SSID);
 }
 
 void wifi_init_sta()
@@ -137,8 +136,7 @@ void wifi_init_sta()
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    ESP_LOGI(TAG, "connect to ap SSID:%s with password", EXAMPLE_ESP_WIFI_SSID);
 }
 
 void app_wifi_main()
--- main/main.c
+++ main/main.c
@@ -26,7 +26,128 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <nvs_flash.h>
+
+#include "app_prov.h"
 #include "birdhouse_peeper.h"
+
+// TODO menuconfig
+#define EXAMPLE_AP_RECONN_ATTEMPTS  5
+#define CONFIG_EXAMPLE_USE_SEC_1
+#define CONFIG_EXAMPLE_USE_POP
+#define CONFIG_EXAMPLE_POP "abcd1234"
+
+
+static const char *TAG = "main";
+
+
+static void start_ble_provisioning();
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int event_id, void* event_data)
+{
+    static int s_retry_num_ap_not_found = 0;
+    static int s_retry_num_ap_auth_fail = 0;
+
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        wifi_event_sta_disconnected_t* disconnected = (wifi_event_sta_disconnected_t*) event_data;
+        switch (disconnected->reason) {
+        case WIFI_REASON_AUTH_EXPIRE:
+        case WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT:
+        case WIFI_REASON_BEACON_TIMEOUT:
+        case WIFI_REASON_AUTH_FAIL:
+        case WIFI_REASON_ASSOC_FAIL:
+        case WIFI_REASON_HANDSHAKE_TIMEOUT:
+            ESP_LOGW(TAG, "connect to the AP fail : auth Error");
+            if (s_retry_num_ap_auth_fail < EXAMPLE_AP_RECONN_ATTEMPTS) {
+                s_retry_num_ap_auth_fail++;
+                esp_wifi_connect();
+                ESP_LOGI(TAG, "retry connecting to the AP...");
+            } else {
+                /* Restart provisioning if authentication fails */
+                start_ble_provisioning();
+            }
+            break;
+        case WIFI_REASON_NO_AP_FOUND:
+            ESP_LOGW(TAG, "connect to the AP fail : not found");
+            if (s_retry_num_ap_not_found < EXAMPLE_AP_RECONN_ATTEMPTS) {
+                s_retry_num_ap_not_found++;
+                esp_wifi_connect();
+                ESP_LOGI(TAG, "retry to connecting to the AP...");
+            }
+            break;
+        default:
+            /* None of the expected reasons */
+            esp_wifi_connect();
+            break;
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->ip_info.ip));
+        s_retry_num_ap_not_found = 0;
+        s_retry_num_ap_auth_fail = 0;
+    }
+}
+
+static void wifi_init_sta()
+{
+    /* Set our event handling */
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, event_handler, NULL));
+
+    /* Start Wi-Fi in station mode with credentials set during provisioning */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+static void start_ble_provisioning()
+{
+    int security = 0; // Security version
+#ifdef CONFIG_EXAMPLE_USE_SEC_1
+    security = 1;
+#endif
+
+    /* Having proof of possession is optional */
+    const protocomm_security_pop_t *pop = NULL;
+#ifdef CONFIG_EXAMPLE_USE_POP
+    const static protocomm_security_pop_t app_pop = {
+        .data = (uint8_t *) CONFIG_EXAMPLE_POP,
+        .len = (sizeof(CONFIG_EXAMPLE_POP)-1)
+    };
+    pop = &app_pop;
+#endif
+
+    ESP_ERROR_CHECK(app_prov_start_ble_provisioning(security, pop));
+}
+
+
+static void app_init()
+{
+    /* Initialize networking stack */
+    tcpip_adapter_init();
+
+    /* Create default event loop needed by the
+     * main app and the provisioning service */
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* Initialize NVS needed by Wi-Fi */
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    /* Initialize Wi-Fi with default config */
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+}
+
 
 void app_main()
 {
@@ -45,17 +166,25 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    // for (int i = 10; i >= 0; i--) {
-    //     printf("Restarting in %d seconds...\n", i);
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // }
-    // printf("Restarting now.\n");
-    // fflush(stdout);
-    // esp_restart();
+    app_init();
 
-    app_wifi_main();
-    app_camera_main();
-    app_httpd_main();
-    app_mdns_main();
-    app_http1_post_jpeg();
+    bool provisioned;
+    if (app_prov_is_provisioned(&provisioned) != ESP_OK) {
+        ESP_LOGE(TAG, "Error getting device provisioning state");
+        return;
+    }
+
+    if (provisioned == false) {
+        ESP_LOGI(TAG, "Starting BLE provisioning");
+        start_ble_provisioning();
+
+    } else {
+        ESP_LOGI(TAG, "Starting WiFi station");
+        wifi_init_sta();
+
+        app_camera_main();
+        app_httpd_main();
+        app_mdns_main();
+        // app_http1_post_jpeg();
+    }
 }
